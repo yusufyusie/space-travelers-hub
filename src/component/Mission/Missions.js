4a3f39c21b0fe81a4eb12e1156ba39f1b13f3e0b
yusufyusie <yesuf023@gmail.com>
2023-09-12 22:56:19 +0300

    Update the mission table structure

--- src/component/Mission/Missions.js
+++ src/component/Mission/Missions.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import './Missions.module.css';
+import style from './Missions.module.css';
 
 const Missions = () => (
   <section className="mission-container">
-    <table className="table">
+    <table className={style.table}>
       <thead>
-        <tr className="row">
-          <th className="column">Mission</th>
-          <th className="column">Description</th>
-          <th className="column">Status</th>
-          <th className="column">Join Mission</th>
+        <tr className={style.row}>
+          <th className={style.column}>Mission</th>
+          <th className={style.column}>Description</th>
+          <th className={style.column}>Status</th>
+          <th className={style.column}>{ ' '}</th>
         </tr>
       </thead>
+      <tbody>
+        <tr>
+          <td className={style.cell}>Thaicom</td>
+          <td className={style.cell}>Thaicom is the name of series of communications satellite</td>
+          <td className={style.cell}>Not A Member</td>
+          <td className={style.cell}>Join Mission</td>
+        </tr>
+      </tbody>
     </table>
   </section>
 );
