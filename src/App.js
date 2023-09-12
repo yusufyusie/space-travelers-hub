ae02420729a3966af37aedda6d557e0296f938f6
yusufyusie <yesuf023@gmail.com>
2023-09-12 16:40:49 +0300

    Add Routing with React Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import Navbar from './component/Navbar/Navbar';
+import Profile from './component/Profile/Profile';
+import Missions from './component/Mission/Missions';
+import Dragons from './component/Dragon/Dragons';
 
 function App() {
   return (
+    <BrowserRouter>
     <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Rockets />} />
+        <Route path="/Missions" element={<Missions />} />
+        <Route path="/Dragons" element={<Dragons />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
