7915b14a8bd4821d4b9fbb5d86e7421b4a237f03
May Pyone <maypyone015@gmail.com>
2023-09-13 16:01:35 +0630

    Add dragon reducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import DragonReducer from './dragon/dragonSlice';
+
 const store = configureStore({
   reducer: {
-   dragon : DragonReducer, 
+    dragon: DragonReducer,
   },
 });
 
