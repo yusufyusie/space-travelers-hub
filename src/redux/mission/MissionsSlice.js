3a2e5c9c724a3b7b28a76a5fc245e23d025ccf93
yusufyusie <yesuf023@gmail.com>
2023-09-13 18:08:27 +0300

    Create a reducer and action dispatcher for join mission

--- src/redux/mission/MissionsSlice.js
+++ src/redux/mission/MissionsSlice.js
@@ -21,7 +21,18 @@ export const getAllMissions = createAsyncThunk(GET_MISSIONS, async (_name, thunk
 const missionsSlice = createSlice({
   name: 'allMissions',
   initialState,
-  reducers: { },
+  reducers: {
+    joinMission: (state, action) => {
+      const newMissionsState = state.allMissions.map((mission) => {
+        if (mission.mission_id !== action.payload) {
+          return mission;
+        }
+        return mission.reserved === true ? { ...mission, reserved: false }
+          : { ...mission, reserved: true };
+      });
+      state.allMissions = newMissionsState;
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(getAllMissions.pending, (state) => {
