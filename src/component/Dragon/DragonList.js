463ac27ca5abc7f49423a376d45da6843e61998b
May Pyone <maypyone015@gmail.com>
2023-09-13 16:03:07 +0630

    Update style

--- src/component/Dragon/DragonList.js
+++ src/component/Dragon/DragonList.js
@@ -1,15 +1,25 @@
-import { fetchDragons, addBook } from "../../redux/dragon/dragonSlice"
-import { useDispatch } from 'react-redux';
+import './dragon.css';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function DrgaonList(){
-
-const dispatch = useDispatch();
-    return (
-        <>
-        <img src = "" alt = ""/>
-        <div>Name</div>
-        <div>Type</div>
-        <button type="button"onClick={()=>dispatch(fetchDragons())}>Reserve Dragon</button>
-        </>
-    )
+export default function DrgaonList(props) {
+  const {
+    id, name, type, image,
+  } = props;
+  return (
+    <li>
+      <div key={id}>
+        <img className="dragonimage" src={image} alt="" />
+        <div>{name}</div>
+        <div>{type}</div>
+        <button type="button">Reserve Dragon</button>
+      </div>
+    </li>
+  );
 }
+DrgaonList.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+};
