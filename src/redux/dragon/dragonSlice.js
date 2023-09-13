13ab1d31c103c4a899a2b3d0220587e3ea553043
May Pyone <maypyone015@gmail.com>
2023-09-13 16:00:01 +0630

    Fetch data from api

--- src/redux/dragon/dragonSlice.js
+++ src/redux/dragon/dragonSlice.js
@@ -1,39 +1,30 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-export const fetchDragons = createAsyncThunk('user/fetchUsers', async () => {
-    try {
-      const response = await axios.get('https://api.spacexdata.com/v4/dragons');
-      return response.data;
-    } catch (error) {
-      return error.message;
-    }
-  });
-  
 const initialState = {
-    totalDragons: [],
-}
-export const dragonSlice = createSlice({
-    name: 'dragon',
-    initialState,
-    reducers: {
-        addBook: () => {
-         console.log("hi");
-        },
-    },
-    extraReducers: (builder) => {
-        builder.addCase(fetchDragons.fulfilled, (state, action) => {
-        console.log("fulfiled");
-        console.log(action.payload);
-        });
-        builder.addCase(fetchDragons.rejected, (state, action) => {
-            console.log(action);
-            });
-            builder.addCase(fetchDragons.pending, (state, action) => {
-                console.log(action);
-                });
-    }
-  });
+  totalDragons: [],
+};
 
-  export const { addBook } = dragonSlice.actions;
+export const fetchDragons = createAsyncThunk('user/fetchUsers', async () => {
+  try {
+    const response = await axios.get('https://api.spacexdata.com/v4/dragons');
+    return response.data;
+  } catch (error) {
+    return error.message;
+  }
+});
+
+export const dragonSlice = createSlice({
+  name: 'dragon',
+  initialState,
+  reducers: {
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchDragons.fulfilled, (state, action) => {
+      state.totalDragons = action.payload;
+    });
+  },
+});
+
+export const { addBook } = dragonSlice.actions;
 export default dragonSlice.reducer;
