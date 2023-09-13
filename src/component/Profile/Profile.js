09f6c74bb48485a01258d9f402b19f100da3c2e8
binyamolango <olangobinyam@gmail.com>
2023-09-13 19:46:43 +0300

    Update style of my profile

--- src/component/Profile/Profile.js
+++ src/component/Profile/Profile.js
@@ -2,28 +2,31 @@
 import { useSelector } from 'react-redux/es/hooks/useSelector';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ListGroup from 'react-bootstrap/ListGroup';
 import Container from 'react-bootstrap/Container';
 import ReservedRocketList from './ReservedRocketList';
+import style from './Profile.module.css';
 
 export default function Profile() {
   const rockets = useSelector((state) => state.rockets.rockets);
 
   return (
-    <Container fluid>
-      <ListGroup>
-        {
-          rockets.map((rocket) => (
-            <ListGroup.Item key={rocket.id}>
-              <ReservedRocketList
-                key={rocket.id}
-                name={rocket.name}
-                reserved={rocket.reserved}
-              />
-            </ListGroup.Item>
-          ))
-        }
-      </ListGroup>
+    <Container fluid className={style.container}>
+      <div className={style.rocketSection}>
+        <h4>My Rockets</h4>
+        <ul className={style.rocketList}>
+          {
+            rockets.map((rocket) => (
+              <li key={rocket.id}>
+                <ReservedRocketList
+                  key={rocket.id}
+                  name={rocket.name}
+                  reserved={rocket.reserved}
+                />
+              </li>
+            ))
+          }
+        </ul>
+      </div>
     </Container>
   );
 }
