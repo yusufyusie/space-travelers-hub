ceeb4abffe43eb135430e1f83adb5aebb5a1c37e
binyamolango <olangobinyam@gmail.com>
2023-09-13 16:54:22 +0300

    Add reserveRocket action

--- src/component/Rocket/RocketsList.js
+++ src/component/Rocket/RocketsList.js
@@ -1,16 +1,24 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
-
 import style from './RocketsList.module.css';
+
+import { reserveRocket } from '../../redux/rocket/rocketSlice';
 
 const RocketsList = (props) => {
   const {
-    name, description, image,
+    name, description, image, id,
   } = props;
+
+  const dispatch = useDispatch();
+
+  const reserveRocketHandler = () => {
+    dispatch(reserveRocket(id));
+  };
 
   return (
     <Col className={style.rocketsContainer}>
@@ -24,7 +32,7 @@ const RocketsList = (props) => {
         <p className={style.rocketType}>
           {description}
         </p>
-        <Button>Reserve Rocket</Button>
+        <Button type="button" onClick={reserveRocketHandler}>Reserve Rocket</Button>
       </div>
     </Col>
   );
@@ -34,7 +42,7 @@ RocketsList.propTypes = {
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
-  // id: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 export default RocketsList;
