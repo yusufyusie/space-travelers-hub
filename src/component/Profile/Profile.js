0837c398606965d8fbce7f557e377f168646fb76
yusufyusie <yesuf023@gmail.com>
2023-09-13 22:15:15 +0300

    Add list table to profile page

--- src/component/Profile/Profile.js
+++ src/component/Profile/Profile.js
@@ -9,7 +9,9 @@ import style from './Profile.module.css';
 
 export default function Profile() {
   const rockets = useSelector((state) => state.rockets.rockets);
+  const { allMissions } = useSelector((store) => store.allMissions);
 
+  const joinedMissions = allMissions.filter((mission) => mission.joined === true);
   return (
     <Container fluid className={style.container}>
       <div className={style.rocketSection}>
@@ -26,6 +28,17 @@ export default function Profile() {
               </ListGroup.Item>
             ))}
         </ListGroup>
+      </div>
+      <div className={style.missionsList}>
+        <h2 className={style.listTitle}>My Missions</h2>
+        {joinedMissions.length === 0
+          ? <p>No joined missions</p> : (
+            <ul>
+              {joinedMissions.map((mission) => (
+                <li key={mission.mission_id}>{mission.mission_name}</li>
+              ))}
+            </ul>
+          )}
       </div>
     </Container>
   );
